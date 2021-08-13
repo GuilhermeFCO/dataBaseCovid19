05273ef4c4ff97baeb6e66dca5fe710cd9e98cef
Guilherme Fernandes Castro de Oliveira <53825689+GuilhermeFCO@users.noreply.github.com>
2021-08-13 10:54:22 -0300

    Update update.R

--- update.R
+++ update.R
@@ -2,3 +2,11 @@ install.packages("crul")
 remotes::install_github(repo = "git@github.com:GuilhermeFCO/covidBR.git")
 covidBR::downloadCovidBR()
 covidBR::createMaps(withCorr = TRUE)
+
+if (!dir.exists("./graphs")
+  dir.create("./graphs")
+
+cities <- readRDS("./rds/cities.rds")
+cities <- cities %>% dplyr::filter(date >= (min(cities$date) + 120))
+saveRDS(cities, "./graphs/cities.rds")
+rm(cities)
